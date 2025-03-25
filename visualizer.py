7a61e692e4d9e04688fd5905933e49d312281b4e
djahan_latran <djahan.latran@gmail.com>
2025-03-25 22:38:08 +0100

    moved board creation to another class. Implemented working draw_board function

--- visualizer.py
+++ visualizer.py
@@ -3,6 +3,7 @@ import pygame as pg
 
 
 class AnimationCanvas:
+
     def __init__(self):
         #Background color
         self.background_cl = (82, 82, 82)
@@ -19,7 +20,7 @@ class AnimationCanvas:
         self.curr_rect_cl = (120, 120, 120)
         self.obstacle_rect_cl = (200, 50, 50)
         self.target_rect_cl = (50, 255, 50)
-
+        self.blue_rect_cl = (95, 245, 250)
         #Label color
         self.label_color = (250, 250, 250)
 
@@ -31,9 +32,6 @@ class AnimationCanvas:
         
         #Bevel on corners of the bars
         self.bar_bevel = 2
-
-        #Size of each rectangle in checkerboard
-        self.rect_size = 16
 
     def create_surface(self, surface_size):
         self.surface_size = surface_size
@@ -75,57 +73,41 @@ class AnimationCanvas:
             value_rect.midbottom = (x_coord + bar_width/2, y_coord)
             self.surface.blit(value_label, value_rect)
     
-    def create_def_board(self):
-        #Create a 2d matrix that represents a checkerboard
-
-        self.checker_rows = self.surface_size[1] // self.rect_size
-        self.checker_cols = self.surface_size[0] // self.rect_size
-
-        #Initialze empty list
-        self.board = []
-
-        #2d list filled with 0s to represent checkerboard structure
-        for _ in range(self.checker_rows):
-            row = []
-            for _ in range(self.checker_cols):
-                row.append(0)
-            self.board.append(row)
-
-        return self.board
-    
-    def draw_board(self, value_info):
+    def draw_board(self, board, value_info= None):
         #Draw the checkerboard
         
         x_coord = 0
         y_coord = 0
 
         #Collect the changes
-        changes = []
-        end_pos = (self.checker_rows, self.checker_cols)
+        #changes = []
+        end_pos = (len(board.raster), len(board.raster[1]))
 
 
-        for i in range(self.checker_rows):
-            for j in range(self.checker_cols):
-                sq_rect = pg.Rect(x_coord, y_coord, self.rect_size, self.rect_size)
-                if self.board[i][j] == 1:
+        for i in range(board.rows):
+            for j in range(board.cols):
+                sq_rect = pg.Rect(x_coord, y_coord, board.rect_size, board.rect_size)
+                if board.raster[i][j] == 1:
                     pg.draw.rect(self.surface, self.obstacle_rect_cl, sq_rect)
-                    changes.append(sq_rect)
+                    #changes.append(sq_rect)
                 elif end_pos and (i, j) == end_pos:
                     pg.draw.rect(self.surface, self.target_rect_cl, sq_rect)
-                    changes.append(sq_rect)
-                elif current and (i, j) == current:
-                    pg.draw.rect(self.surface, self.curr_rect_cl, sq_rect, 3)
-                    changes.append(sq_rect)
-                elif visited and (i, j) in visited:
+                    #changes.append(sq_rect)
+                elif value_info and value_info["current"] and (i, j) == value_info["current"]:
+                    pg.draw.rect(self.surface, self.blue_rect_cl, sq_rect, 3)
+                    #changes.append(sq_rect)
+                elif value_info and value_info["visited"] and (i, j) in value_info["visited"]:
                     pg.draw.rect(self.surface, self.curr_rect_cl, sq_rect)
-                    pg.draw.rect(self.surface, self.def_rect_cl, sq_rect, 1)
-                    changes.append(sq_rect)
+                    pg.draw.rect(self.surface, self.def_rect_bord_cl, sq_rect, 1)
+                    #changes.append(sq_rect)
                 else:
                     pg.draw.rect(self.surface, self.def_rect_cl, sq_rect)
                     pg.draw.rect(self.surface, self.def_rect_bord_cl, sq_rect, 1)
-                    changes.append(sq_rect)
+                    #changes.append(sq_rect)
 
-                x_coord += self.rect_size
+                x_coord += board.rect_size
 
-            y_coord += self.rect_size
+            y_coord += board.rect_size
             x_coord = 0 
+        
+        
