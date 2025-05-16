9321ffa8e21e4ee42326a385c2b184373289770a
djahan_latran <djahan.latran@gmail.com>
2025-05-16 16:29:47 +0200

    moved many parameters from other classes to AppStates to cleaner seperate concerns

--- app_states.py
+++ app_states.py
@@ -3,17 +3,35 @@
 
 class AppStates:
     def __init__(self):
-        self.running = False
+        self.app_running = False
         self.algo_running = False
+
+        self.surface_update_intv = 1/20
+        self.gui_update_intv = 1/60
+        self.surface_updated = 0
+        self.gui_updated = 0
 
         self.algo_generator = None
 
         self.selected_algo = None
         self.curr_algo_cat = None
         self.value_to_find = None
+        self.values = None
 
         self.target_sel_phase = False
         self.target_selected = False
 
         self.obstacle_sel_phase = False
         self.obstacle_selected = False
+
+        self.checkerboard = None
+
+        self.parameter_reset = False
+        self.board_rect_amount = (34, 20) #should make animation panel pass that info to controller to create board
+        self.anim_surf_size = (680, 400)
+
+        self.next_algo_step = False
+        self.next_animation_frame = False
+
+        self.draw_bg_info = {"positive": [], "neutral": [], "negative": []}
+        self.draw_pf_info = {"current": None, "visited": None}
